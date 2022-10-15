2dd637658f6760c16aa9a2ea4f379fbefa901d05
Dmitry <dep1982@mail.ru>
2022-10-15 19:33:32 +0300

    Zadacha10

---
+++ DZ10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+int InCenter(int number)
+{   
+    int result = ((number / 10) % 10);
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число");
+
+int number = int.Parse(Console.ReadLine());
+int size = number.ToString().Length;
+
+if (size == 3 )
+{
+    Console.WriteLine(InCenter(number));
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число");   
+}
